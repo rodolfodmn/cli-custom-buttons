94b70d521395a19297b011ebfc3c52765bd471b7
rodolfo domingos <rodolfo.dmn@gmail.com>
2021-01-10 19:47:42 -0300

    add custom css nas paginas

---
+++ CliCustom/Buttons/Block/CustomColor.php
@@ -0,0 +1,10 @@
+<?php
+namespace CliCustom\Buttons\Block;
+
+class CustomColor extends \Magento\Framework\View\Element\Template
+{
+    public function getTitle()
+    {
+        return "Custom Color";
+    }
+}
